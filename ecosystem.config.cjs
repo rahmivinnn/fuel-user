a11d80ee881e37a93f44778c08cf7e67c207842e
1wasilah1 <m.wasilahhadi@gmail.com>
2025-12-18 18:20:06 +0700

    change ecosystem config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,11 +5,21 @@ module.exports = {
     cwd: '/root/fuel-user',
     env: {
       NODE_ENV: 'development',
-      PORT: 4000
+      PORT: 4000,
+      RESEND_API_KEY: 're_dqtrUVUR_7F6Q46KKxviBmf1YFRgeUPep',
+      TWILIO_ACCOUNT_SID: 'SK9ffb759f2f6aaac4f065ae673e7b0ffc',
+      TWILIO_AUTH_TOKEN: '40e356431aadcc5eae075e9c80cde595',
+      TWILIO_MAIN_ACCOUNT_SID: 'AC_YOUR_MAIN_ACCOUNT_SID_HERE',
+      TWILIO_PHONE_NUMBER: '+6282261402001'
     },
     env_production: {
       NODE_ENV: 'production',
-      PORT: 4000
+      PORT: 4000,
+      RESEND_API_KEY: 're_dqtrUVUR_7F6Q46KKxviBmf1YFRgeUPep',
+      TWILIO_ACCOUNT_SID: 'SK9ffb759f2f6aaac4f065ae673e7b0ffc',
+      TWILIO_AUTH_TOKEN: '40e356431aadcc5eae075e9c80cde595',
+      TWILIO_MAIN_ACCOUNT_SID: 'AC_YOUR_MAIN_ACCOUNT_SID_HERE',
+      TWILIO_PHONE_NUMBER: '+6282261402001'
     },
     instances: 1,
     autorestart: true,
