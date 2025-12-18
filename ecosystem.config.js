7e88ff8b8b5c196231b69bcc3cea4b0dc0dcef1a
1wasilah1 <m.wasilahhadi@gmail.com>
2025-12-18 18:13:57 +0700

    add ecosystem to pm2 deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [{
+    name: 'fuel-user-api',
+    script: 'server/index.js',
+    cwd: '/Users/admin/Documents/FREELANCE/fuel-user',
+    env: {
+      NODE_ENV: 'development',
+      PORT: 4000
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 4000
+    },
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    log_file: './logs/combined.log',
+    out_file: './logs/out.log',
+    error_file: './logs/error.log',
+    time: true
+  }]
+};
